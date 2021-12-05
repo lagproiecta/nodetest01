aa7937a26e7e533b38ba872116876dd3e8006d23
lagproiecta <lastudillo@proiecta.cl>
2021-12-05 00:01:30 -0300

    texto modificado

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app=express();
-app.get('/',(req,res)=>res.send('LAG Hola mundo con express'))
+app.get('/',(req,res)=>res.send('LAG Hola mundo con express SEGUNDA VERSION!!'))
 
 app.listen(3000);
-console.log('Server on port 3000 LAG');
+console.log('Server on port 3000 LAG2 ');
